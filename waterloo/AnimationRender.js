00cdaa578e279070a6288d07c18309e2b5cbff34
AndriiYukhymchak <a.yukhymchak@gmail.com>
2013-09-13 12:06:07 -0700

    Global timer for animations
    AnimationRender.js - added global current tick timer. This is the first step to make this optimization work

--- waterloo/AnimationRender.js
+++ waterloo/AnimationRender.js
@@ -2,23 +2,24 @@ WE.define('Waterloo.Graphics.AnimationRender', Waterloo.Module,{
 	tcode: 'animationrender',
 	
 	_objectsByLayers: {},
+	_frameTimer: 0,
 	
 	doRender: function(e, _input, _output){
 		if(!(_input.item instanceof Waterloo.Graphics.AnimationInstance)) return;
 		var anim = _input.item;
 		var layer = _input.layer;
-		var obj = anim.getFrame(new Date());
+		var obj = anim.getFrame(this._frameTimer);
 		e.reApply(_output.todo, layer, obj);
 	},
 	
 	doBeforeDrawStart: function(e, _input, _output){
+		this._frameTimer = (new Date())-0;
 		if(_input.needsRedraw()) return;
 		for(var x in _input.objects){
 			var obj = _input.objects[x];
 			if(!(obj instanceof Waterloo.Graphics.AnimationInstance))
 				continue;
-			var cframe = new Date();
-			if(obj._nextframetime<=cframe){
+			if(obj._nextframetime<=this._frameTimer){
 				_input.invalidate();
 				return;
 			}
@@ -41,7 +42,7 @@ WE.define('Waterloo.Graphics.AnimationRender', Waterloo.Module,{
 	
 	doRenderLayer: function(e, _input, _output){
 		var lr = _input.layer;
-		var time = new Date();
+		var time = this._frameTimer;
 		var objs = this._objectsByLayers[lr.id];
 		for(var x in objs){
 			var xx = objs[x];
